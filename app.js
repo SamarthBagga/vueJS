afed6db5b1a4565855b85b98aca2a80de2421001
SamarthBagga <samarthbagga@gmail.com>
2024-02-09 00:06:30 +0530

    random user complete with api fetch

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const app = Vue.createApp({
           }
         },
         methods: {
-            getUser() {
-                this.firstName= 'Samarth',
-                this.lastName= 'Bagga',
-                this.email= 'sam@gmail.com',
-                this.gender= 'male',
-                this.picture= 'https://randomuser.me/api/portraits/men/9.jpg'
+            async getUser() {
+                const res = await fetch('https://randomuser.me/api')
+                const { results } = await res.json()
+
+                this.firstName= results[0].name.first
+                this.lastName= results[0].name.last
+                this.email= results[0].email
+                this.gender= results[0].gender
+                this.picture= results[0].picture.large
             },
         }
 })
