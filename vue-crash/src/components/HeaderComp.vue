680e3c64f444ac6c9a607df6aca4104a37964640
SamarthBagga <samarthbagga@gmail.com>
2024-02-10 19:24:47 +0530

    frontend done

---
+++ vue-crash/src/components/HeaderComp.vue
@@ -0,0 +1,30 @@
+<script>
+import ButtonComp from './ButtonComp.vue'
+
+export default {
+    name: 'HeaderComp',
+    props:{
+        title:String,
+        showAddTask:Boolean
+    },
+    components: {
+        ButtonComp
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h1>{{title}} </h1>
+        <ButtonComp @btn-click = "$emit('toggle-add-task')" :text = "showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'"/>
+    </header>
+</template>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+    margin-bottom: 20px;
+}
+</style>
---
+++ vue-crash/src/components/TasksComp.vue
@@ -0,0 +1,20 @@
+<script>
+import TaskComp from './TaskComp.vue';
+
+  export default {
+    name: 'TasksComp',
+    props: {
+        tasks: Array
+    },
+    components: { TaskComp },
+    emits:['delete-task', 'toggle-reminder'],
+}
+    </script>
+<template>
+    <div v-for="task in tasks" :key="task.id">
+      <TaskComp @toggle-reminder="$emit('toggle-reminder',task.id)" @delete-task="$emit('delete-task',task.id)" :task = "task"/>
+    </div>
+  </template>
+  
+
+  
