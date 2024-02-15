8263e0b392f79e550113bf85325511ed16eacdaa
SamarthBagga <samarthbagga@gmail.com>
2024-02-16 00:55:04 +0530

    3d stuf

---
+++ 3d/threejs-vue/src/pages/Index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { PerspectiveCamera, Scene } from 'three';
+
+const scene = new Scene();
+
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+scene.add(camera);
+</script>
+
+<template></template>
