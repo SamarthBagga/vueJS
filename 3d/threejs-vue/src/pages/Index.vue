86f1a3860a7f57e065bda4e10c65e34becdb0441
SamarthBagga <samarthbagga@gmail.com>
2024-02-24 01:37:18 +0530

    get the bag

--- 3d/threejs-vue/src/pages/Index.vue
+++ 3d/threejs-vue/src/pages/Index.vue
@@ -1,15 +1,60 @@
 <script setup lang="ts">
-import { PerspectiveCamera, Scene } from 'three';
+import { useWindowSize } from '@vueuse/core';
+import { Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, SphereGeometry, WebGL1Renderer } from 'three';
+import { computed, onMounted, ref, watch } from 'vue';
 
+let renderer: WebGL1Renderer;
+let camera: PerspectiveCamera;
+const experience = ref<HTMLCanvasElement | null>(null);
 const scene = new Scene();
 
-const camera = new PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
+const { width, height } = useWindowSize();
+
+const aspectRatio = computed(() => width.value / height.value);
+
+function updateRenderer() {
+  renderer.setSize(width.value, height.value);
+  renderer.setPixelRatio(window.devicePixelRatio);
+}
+
+function updateCamera() {
+  camera.aspect = aspectRatio.value;
+  camera.updateProjectionMatrix();
+}
+
+watch(aspectRatio, updateRenderer);
+watch(aspectRatio, updateCamera);
+
+camera = new PerspectiveCamera(
+  45,
+  aspectRatio.value,
   0.1,
   1000,
 );
+
+camera.position.z = 5;
 scene.add(camera);
+
+const sphere = new Mesh(
+  new SphereGeometry(1, 20, 20),
+  new MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+scene.add(sphere);
+
+onMounted(() => {
+  renderer = new WebGL1Renderer({
+    canvas: experience.value as HTMLCanvasElement,
+    antialias: true,
+  });
+
+  renderer.setSize(width.value, height.value);
+
+  renderer.render(scene, camera);
+});
 </script>
 
-<template></template>
+<template>
+  <!-- Use ":ref" for binding -->
+  <canvas ref="experience" />
+</template>
